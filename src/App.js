0562b58d19945ae956f1989d699e80047e2bfe95
shyali <shyali.narayan@gmail.com>
2023-01-10 22:36:26 +0530

    adding all the files

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Heading from "./Heading.js";
+import Footer from "./Footer.js";
+
+function App(){
+    return(
+        <div>
+            <Heading/>
+            <Footer/>
+        </div>
+    )
+}
+  export default App;
